bc0c897d554d4178766ce76492c7e718f0121917
BlojuP <BlojuP@users.noreply.github.com>
2022-01-09 14:58:10 +0200

    Update adeverinte.py

--- adeverinte.py
+++ adeverinte.py
@@ -1,8 +1,12 @@
 from tkinter import *
 from tkcalendar import *
+from datetime import *
+from dateutil.relativedelta import *
+from dateutil.parser import parse
 import tkinter.messagebox
 import sqlite3
 
+expira = date.today()
 
 #define Adauga window
 def adauga_win():
@@ -17,11 +21,13 @@ def adauga_win():
             self.lbl3=Label(window, text='Email')
             self.lbl4=Label(window, text='Telefon')
             self.lbl5=Label(window, text='Data')
+            self.lbl6=Label(window, text='Expira')
             self.t1=Entry(window, bd=3)
             self.t2=Entry(window, bd=3)
             self.t3=Entry(window, bd=3)
             self.t4=Entry(window, bd=3)
             self.t5= my_label
+            self.t6= my_sixlabel
             self.btn1 = Button(window, text='Adauga')
             self.btn2 = Button(window, text='Iesire')
             self.lbl1.place(x=50, y=70)
@@ -34,6 +40,8 @@ def adauga_win():
             self.t4.place(x=120, y=220)
             self.lbl5.place(x=50, y=270)
             self.t5.place(x=120, y=270)
+            self.lbl6.place(x=50, y=320)
+            self.t6.place(x=120, y=320)
             self.b1=Button(window, text='Adauga', command=self.add)
             self.b2=Button(window, text='Iesire', command=window.destroy)
             self.b1.place(x=50, y=350)
@@ -42,8 +50,8 @@ def adauga_win():
    
         def add(self):
             #sql = "INSERT INTO adeverinte(nume, prenume, email, telefon, data) VALUES (?, ?, ?, ?, ?);"
-            textlist = [self.t1.get() , self.t2.get(), self.t3.get(), self.t4.get(), cal.get_date()]
-            cur.execute("""INSERT INTO adeverinte (nume, prenume, email, telefon, data) VALUES (?, ?, ?, ?, ?); """, textlist)
+            textlist = [self.t1.get() , self.t2.get(), self.t3.get(), self.t4.get(), cal.get_date(), expira]
+            cur.execute("""INSERT INTO adeverinte (nume, prenume, email, telefon, data, expira) VALUES (?, ?, ?, ?, ?, ?); """, textlist)
             con.commit()
             
             joined_string = " | ".join(textlist)
@@ -54,21 +62,31 @@ def adauga_win():
                 outF.write(line)
                 
             outF.close()
-
-    cal = Calendar(window, selectmode="day", year=2021, month=9, day=7)
+#Today's Date Calendar
+    today = date.today()
+    cal = Calendar(window, selectmode="day", year=today.year, month=today.month, day=today.day)
     cal.pack(anchor=NE, padx=50, pady=50)
-
-    def grab_date():
-        my_label.config(text=cal.get_date())
         
+    def grab_date():
+        global expira
+        my_label.config(text=cal.get_date()) #selected date
+        #calculate selected date + 6 months
+        dt = cal.get_date()
+        date1 = datetime.strptime(dt, "%m/%d/%y")
+        date_expira = date1 + relativedelta(months=+6)
+        expira = date_expira.strftime("%m/%d/%y")
+        my_sixlabel.config(text=expira)
+        
+    
+    
 
-    my_button = Button(window, text="Selecteaza Data Expirare", command=grab_date)
+    my_button = Button(window, text="Selecteaza Data Expirare", command = grab_date)
     my_button.pack(anchor=E, padx=100)
 
-
+    
     my_label = Label(window, text="")
-
-            
+    my_sixlabel = Label(window, text= "")
+                
     mywin=MyWindow(window)
 
 
@@ -86,7 +104,7 @@ con = sqlite3.connect("adeverinte.db")
 # preparing a cursor object
 cur = con.cursor()
 
-cur.execute("""create table if not exists adeverinte(ROWID INTEGER PRIMARY KEY, nume VARCHAR(200) not null, prenume VARCHAR(200) not null, email VARCHAR(200), telefon INT, data DATE);""")
+cur.execute("""create table if not exists adeverinte(ROWID INTEGER PRIMARY KEY, nume VARCHAR(200) not null, prenume VARCHAR(200) not null, email VARCHAR(200), telefon INT, data DATE, expira DATE);""")
 
 
 #Expired entries
@@ -112,7 +130,7 @@ def file_expirate():
             print("Total rows are: ", len(records))
             print("Printing each row")
             for row in records:
-                print(row[1], row[2], row[3], row[4], row[5])
+                print(row[1], row[2], row[3], row[4], row[5], row[6])
                 print("\n")
 
             cursor.close()
