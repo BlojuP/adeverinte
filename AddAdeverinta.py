6a72c0801f489824c45a291848ec23b54e5f8dd5
BlojuP <37499177+BlojuP@users.noreply.github.com>
2022-08-08 11:29:33 +0300

    Add files via upload

--- AddAdeverinta.py
+++ AddAdeverinta.py
@@ -41,7 +41,7 @@ def addAdev():
     root = Tk()
     root.title("Adeverinte")
     root.minsize(width=400,height=400)
-    root.geometry("1200x500")
+    root.geometry("1200x600")
 
     con = sqlite3.connect("data/adeverinte.db")
     cur = con.cursor()
@@ -53,64 +53,43 @@ def addAdev():
     Canvas1.config(bg="#ff6e40")
     Canvas1.pack(expand=True,fill=BOTH)
 
+#Frame2
+    #labelFrame2 = Frame(root,bg='black')
+    #labelFrame2.place(relx=0.6,rely=0.25,relwidth=0.27,relheight=0.6)
+    #today = date.today()
+    #cal = Calendar(labelFrame2, selectmode="day", year=today.year, month=today.month, day=today.day)
+    #cal.place(relx=0.05,rely=0.04, relheight=0.6) 
+        
 
- #Frame1               
+    #def grab_date():
+        #global expira1, dt
+        #my_label.config(text=cal.get_date()) #selected date
+        #dt = cal.get_date()
+        #date1 = datetime.strptime(dt, "%m/%d/%y")
+        #date_expira = date1 + relativedelta(months=+6)
+        #expira1 = date_expira.strftime("%B/%y")
+        #my_sixlabel.config(text=expira1)
+        #my_label.config(text="Data emitere" + cal.get_date())
+
+    #my_button = Button(labelFrame2, text="Get Date", command = grab_date)
+    #my_button.place(relx=0.35,rely=0.67)
+
+    #my_label = Label(labelFrame2, text="")
+    #my_label.place(relx=0.3,rely=0.95, relwidth=0.62, relheight=0.08)
+    #my_sixlabel = Label(labelFrame2, text= "")
+    #my_sixlabel.place(relx=0.3,rely=0.105, relwidth=0.62, relheight=0.08)
+
+#Frame1               
     headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
     headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
     headingLabel = Label(headingFrame1, text="Adauga Adeverinte", bg='black', fg='white', font=('Courier',15))
     headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
     labelFrame = Frame(root,bg='black')
-    labelFrame.place(relx=0.1,rely=0.45,relwidth=0.5,relheight=0.4)
-                
-    # Nume
-    lb1 = Label(labelFrame,text="Nume : ", bg='black', fg='white')
-    lb1.place(relx=0.05, rely=0.10, relheight=0.08)
-                
-    adevInfo1 = Entry(labelFrame)
-    adevInfo1.place(relx=0.3 ,rely=0.10, relwidth=0.62, relheight=0.08)
-                
-    # Prenume
-    lb2 = Label(labelFrame,text="Prenume : ", bg='black', fg='white')
-    lb2.place(relx=0.05,rely=0.25, relheight=0.08)
-                
-    adevInfo2 = Entry(labelFrame)
-    adevInfo2.place(relx=0.3,rely=0.25, relwidth=0.62, relheight=0.08)
-                
-    # Email
-    lb3 = Label(labelFrame,text="Email : ", bg='black', fg='white')
-    lb3.place(relx=0.05,rely=0.40, relheight=0.08)
-                
-    adevInfo3 = Entry(labelFrame)
-    adevInfo3.place(relx=0.3,rely=0.40, relwidth=0.62, relheight=0.08)
-                
-    # Telefon
-    lb4 = Label(labelFrame,text="Telefon : ", bg='black', fg='white')
-    lb4.place(relx=0.05,rely=0.55, relheight=0.08)
-                
-    adevInfo4 = Entry(labelFrame)
-    adevInfo4.place(relx=0.3,rely=0.55, relwidth=0.62, relheight=0.08)
-
-    # Emitere
-    lb5 = Label(labelFrame,text="Data Emitere : ", bg='black', fg='white')
-    lb5.place(relx=0.05,rely=0.70, relheight=0.08)
-                
-    adevInfo5 = dt
-    adevInfo5.place(relx=0.3,rely=0.70, relwidth=0.62, relheight=0.08)
-
-    # Expira
-    lb6 = Label(labelFrame,text="Data Expira : ", bg='black', fg='white')
-    lb6.place(relx=0.05,rely=0.85)
-                
-    adevInfo6 = expira1
-    adevInfo6.place(relx=0.3,rely=0.85, relwidth=0.62, relheight=0.08)
-
-#Frame2
-    labelFrame2 = Frame(root,bg='black')
-    labelFrame2.place(relx=0.6,rely=0.45,relwidth=0.27,relheight=0.4)
+    labelFrame.place(relx=0.1,rely=0.25,relwidth=0.8,relheight=0.5)
     today = date.today()
-    cal = Calendar(labelFrame2, selectmode="day", year=today.year, month=today.month, day=today.day)
-    cal.place(relx=0.05,rely=0.04, relheight=0.8) 
-        
+    cal = Calendar(labelFrame, selectmode="day", year=today.year, month=today.month, day=today.day)
+    cal.place(relx=0.70,rely=0.04, relheight=0.6) 
+
 
     def grab_date():
         global expira1, dt
@@ -122,13 +101,64 @@ def addAdev():
         my_sixlabel.config(text=expira1)
         #my_label.config(text="Data emitere" + cal.get_date())
 
-    my_button = Button(labelFrame2, text="Get Date", command = grab_date)
-    my_button.place(relx=0.35,rely=0.87)
+    my_button = Button(labelFrame, text="Get Date", command = grab_date)
+    my_button.place(relx=0.80,rely=0.67)
 
-    my_label = Label(labelFrame2, text="")
-    #my_label.place(relx=0.3,rely=0.70, relwidth=0.62, relheight=0.08)
-    my_sixlabel = Label(labelFrame2, text= "")
-    #my_sixlabel.place(relx=0.3,rely=0.85, relwidth=0.62, relheight=0.08)
+                
+    # Nume
+    lb1 = Label(labelFrame,text="Nume : ", bg='black', fg='white')
+    lb1.place(relx=0.05, rely=0.10, relheight=0.08)
+                
+    adevInfo1 = Entry(labelFrame)
+    adevInfo1.place(relx=0.3 ,rely=0.10, relwidth=0.35, relheight=0.08)
+                
+    # Prenume
+    lb2 = Label(labelFrame,text="Prenume : ", bg='black', fg='white')
+    lb2.place(relx=0.05,rely=0.25, relheight=0.08)
+                
+    adevInfo2 = Entry(labelFrame)
+    adevInfo2.place(relx=0.3,rely=0.25, relwidth=0.35, relheight=0.08)
+                
+    # Email
+    lb3 = Label(labelFrame,text="Email : ", bg='black', fg='white')
+    lb3.place(relx=0.05,rely=0.40, relheight=0.08)
+                
+    adevInfo3 = Entry(labelFrame)
+    adevInfo3.place(relx=0.3,rely=0.40, relwidth=0.35, relheight=0.08)
+                
+    # Telefon
+    lb4 = Label(labelFrame,text="Telefon : ", bg='black', fg='white')
+    lb4.place(relx=0.05,rely=0.55, relheight=0.08)
+                
+    adevInfo4 = Entry(labelFrame)
+    adevInfo4.place(relx=0.3,rely=0.55, relwidth=0.35, relheight=0.08)
+
+    # Emitere
+    #lb5 = Label(labelFrame,text="Data Emitere : ", bg='black', fg='white')
+    #lb5.place(relx=0.05,rely=0.70, relheight=0.08)
+                
+    #adevInfo5 = dt
+    #adevInfo5.place(relx=0.3,rely=0.70, relwidth=0.35, relheight=0.08)
+
+    # Expira
+    #lb6 = Label(labelFrame,text="Data Expira : ", bg='black', fg='white')
+    #lb6.place(relx=0.05,rely=0.85)
+                
+    #adevInfo6 = expira1
+    #adevInfo6.place(relx=0.3,rely=0.85, relwidth=0.35, relheight=0.08)
+
+    # Emitere
+    lb5 = Label(labelFrame,text="Data Emitere : ", bg='black', fg='white')
+    lb5.place(relx=0.05,rely=0.70, relheight=0.08)
+    my_label = Label(labelFrame, text="", bg='black', fg='white')
+    my_label.place(relx=0.3,rely=0.70, relwidth=0.35, relheight=0.08)
+
+    # Expira
+    lb6 = Label(labelFrame,text="Data Expira : ", bg='black', fg='white')
+    lb6.place(relx=0.05,rely=0.85)
+    my_sixlabel = Label(labelFrame, text="", bg='black', fg='white')
+    my_sixlabel.place(relx=0.3,rely=0.85, relwidth=0.35, relheight=0.08)
+
     
 
 
